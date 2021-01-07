5e6800a93273a8977599173b80e9b35111ab046e
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-06 22:03:26 -0500

    Removed relationship
    Co-Authored-By: Kiera <kieraaallen@gmail.com>
    Co-Authored-By: Joshua Carey <jdcarey128@users.noreply.github.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -1,5 +1,4 @@
 class Footprint < ApplicationRecord
-  belongs_to :car_monthly_mileage
   validates :carbon_in_kg,
             :offset_cost_total,
             :offset_cost_currency, presence: true
