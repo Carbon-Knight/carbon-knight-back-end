c4534d75499e1a55796d22d72994a9eaec9bf7b9
Joshua Carey <jdcarey128@gmail.com>
2021-01-06 19:05:58 -0700

    Define friendship and car types and inputs for GraphQL
    Co-authored-by: Jonathan Wilson <53539820+Jonathan-M-Wilson@users.noreply.github.com>

---
+++ app/graphql/types/car_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class CarType < Types::BaseObject
+    field :id, ID, null: false
+    field :user_id, Integer, null: false
+    field :make, String, null: true
+    field :model, String, null: true
+    field :year, Integer, null: true
+    field :mpg, Integer, null: false
+    field :fuel_type, String, null: false
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,10 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    # How to define query types: 
+    # field <:query_name>, resolver: Queries::ClassName(defined in query_file.rb)
+    # resolver: executes the logic in the resolve method of the above class 
+
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
