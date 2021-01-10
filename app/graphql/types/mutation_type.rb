cca4a77878a9f97b7aa150ee09849627c76bae8c
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-10 14:13:13 -0700

    Add create footprint and car monthly mileage field to mutation type
    Co-Authored-By: Curtis B <c-bartell@users.noreply.github.com>
    Co-Authored-By: Eugene Theriault <ETBassist@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user_car, mutation: Mutations::CreateUserCar
+    field :create_footprint_and_car_monthly_mileage, mutation: Mutations::CreateFootprintAndCarMonthlyMileage
   end
 end
