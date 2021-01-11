3467239c412ae0f3dc19e5db2ef38f1ea8927ec7
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-10 22:05:43 -0500

    Added the aggregate_footprint_for_month mothod to the facade that returns a hash of the aggregate footprint

--- app/facades/footprint_facade.rb
+++ app/facades/footprint_facade.rb
@@ -8,5 +8,15 @@ class FootprintFacade
         offset_cost_currency: response[:cloverly_offset_cost][:currency]
       )
     end
+
+    def aggregate_footprint_for_month(user_id, month, year)
+      aggregate_footprint = CarMonthlyMileage.joins(:car, :footprint)
+      .select("user_id","carbon_in_kg")
+      .where("LOWER(month) = ?", "#{month.downcase}")
+      .where(year: year, cars: {user_id: user_id})
+      .sum("carbon_in_kg")
+
+      {carbon_in_kg: aggregate_footprint}
+    end
   end
 end
