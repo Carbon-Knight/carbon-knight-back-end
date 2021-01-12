72a9d873412b5a65b228fe5007c9fc8a806b2610
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-12 18:08:01 -0500

    Removed Aggregate Footprint for Month traces
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- app/graphql/types/aggregate_footprint_for_month_type.rb
+++
@@ -1,5 +0,0 @@
-module Types
-  class AggregateFootprintForMonthType < Types::BaseObject
-    field :carbon_in_kg, Float, null: false
-  end
-end
--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -5,20 +5,6 @@ class Footprint < ApplicationRecord
             :offset_cost_total,
             :offset_cost_currency, presence: true
 
-  def self.aggregate_footprint_for_month(user_id, month, year)
-    #If we add another service(flight?)
-    #it would have to be added in the the second where method if the flight
-    #table also has a carbon_in_kg column if not this active record
-    #call may have to be redone
-
-    aggregate_footprint = Footprint.joins(car_monthly_mileage: :car)
-    .where("LOWER(month) = ?", "#{month.downcase}")
-    .where(car_monthly_mileages: {year: year}, cars: {user_id: user_id})
-    .sum('carbon_in_kg')
-
-    {carbon_in_kg: aggregate_footprint}
-  end
-
   def self.aggregate_footprint_for_year(user_id, year)
     Footprint.joins(car_monthly_mileage: :car)
     .select('car_monthly_mileages.month')
