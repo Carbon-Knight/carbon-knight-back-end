a0c7ccb30468a62ec5bf0a9e36d5cbb3e8ff3082
Roberto Basulto <roberto.basulto085@gmail.com>
2021-01-11 19:47:07 -0500

    Renamed files and moved test out of facade
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>

--- app/facades/footprint_facade.rb
+++ app/facades/footprint_facade.rb
@@ -8,20 +8,5 @@ class FootprintFacade
         offset_cost_currency: response[:cloverly_offset_cost][:currency]
       )
     end
-
-    def aggregate_footprint_for_month(user_id, month, year)
-      #If we add another service(flight?)
-      #it would have to be add the the second where method if the flight
-      #table also has a carbon_in_kg column if not this active record
-      #call may have to be redone
-
-      aggregate_footprint = CarMonthlyMileage.joins(:car, :footprint)
-      .select("user_id","carbon_in_kg")
-      .where("LOWER(month) = ?", "#{month.downcase}")
-      .where(year: year, cars: {user_id: user_id})
-      .sum("carbon_in_kg")
-
-      {carbon_in_kg: aggregate_footprint}
-    end
   end
 end
--- app/graphql/types/aggregate_footprint_type.rb
+++
@@ -1,5 +0,0 @@
-module Types
-  class AggregateFootprintType < Types::BaseObject
-    field :carbon_in_kg, Float, null: false
-  end
-end
