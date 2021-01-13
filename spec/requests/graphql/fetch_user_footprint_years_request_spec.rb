370dd6eacffd7cdf2a67b8781f7724a115b317be
Curtis Bartell <cbartell980@gmail.com>
2021-01-13 16:10:47 -0700

    Add test for fetchUserFootprintYears request

--- spec/requests/graphql/fetch_user_footprint_years_request_spec.rb
+++ spec/requests/graphql/fetch_user_footprint_years_request_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe 'Fetch all years where a user had a carbon footprint' do
   it 'can fetch years for a user' do
     user_id = 1
-    car_1 =
+    other_user_id = 2
+
+    user_cars = create_list(:car, 2, :with_records, user_id: user_id)
+    other_cars = create_list(:car, 2, :with_records, user_id: other_user_id)
+
     query_string = <<-GRAPHQL
       query {
         fetchUserFootprintYears(userId: #{user_id}){
@@ -13,8 +17,19 @@ describe 'Fetch all years where a user had a carbon footprint' do
     GRAPHQL
 
     post graphql_path, params: { query: query_string}
+    binding.pry
+    expect(response).to be_successful
+
     result = JSON.parse(response.body, symbolize_names: true)
 
-    
+    expect(result).to be_a Hash
+    expect(result).to have_key :data
+    expect(result[:data]).to be_a Hash
+
+    data = result[:data]
+
+    expect(data).to have_key :fetchUserFootprintYears
+    expect(data[:fetchUserFootprintYears]).to be_an Array
+    expect(data[:fetchUserFootprintYears]).to eq CarMonthlyMileage.fetch_user_years(user_id)
   end
 end
