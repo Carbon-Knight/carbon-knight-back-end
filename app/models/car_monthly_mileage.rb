0db3195a290f2ef27f4a571aedb9a30c191393a7
Curtis Bartell <cbartell980@gmail.com>
2021-01-13 15:54:49 -0700

    Add fetch_user_years and refactor with class << self
    Co-authored-by: James Belta <41242546+JBelta@users.noreply.github.com>

--- app/models/car_monthly_mileage.rb
+++ app/models/car_monthly_mileage.rb
@@ -9,7 +9,18 @@ class CarMonthlyMileage < ApplicationRecord
             :year, presence: true
 
 
-  def self.fetch_all_user_car_monthly_mileages(user_id)
-    CarMonthlyMileage.joins(:car).where(cars: {user_id: user_id})
+  class << self
+    def fetch_all_user_car_monthly_mileages(user_id)
+      joins(:car).where(cars: {user_id: user_id})
+    end
+
+    def fetch_user_years(user_id)
+      select(car_monthly_mileages: :year)
+        .joins(:car)
+        .where(cars: {user_id: user_id})
+        .distinct(car_monthly_mileages: :year)
+        .order(year: :asc)
+        .pluck(:year)
+    end
   end
 end
