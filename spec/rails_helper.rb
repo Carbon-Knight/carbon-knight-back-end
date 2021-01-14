ee8ff1a1a171bac1df76fa340a1c9339ea0fdb45
Curtis Bartell <cbartell980@gmail.com>
2021-01-13 20:02:10 -0700

    Add SimpleCov filter for graphql controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
 SimpleCov.start
+SimpleCov.add_filter ['app/controllers/graphql_controller.rb']
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
