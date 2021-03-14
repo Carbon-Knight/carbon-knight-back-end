586a0ba5ae8d0f14b667939c10d32aae47be2607
Eugene Theriault <hybridbassist@gmail.com>
2021-03-14 11:18:39 -0700

    Create test for car field on car monthly mileage
    Tests that you can query a related car through a car monthly mileage

--- spec/requests/graphql/car_monthly_mileage_request_spec.rb
+++ spec/requests/graphql/car_monthly_mileage_request_spec.rb
@@ -36,16 +36,37 @@ describe 'CarMonthlyMileageQuery' do
           month
           year
           totalMileage
+          car {
+            id
+            make
+            model
+            year
+            mpg
+            fuelType
+          }
         }
       }
     GRAPHQL
 
 
     post graphql_path, params: { query: query_string }
+    require "pry";binding.pry 
     result = JSON.parse(response.body, symbolize_names: true)
     expect(result[:data][:carMonthlyMileage][:id]).to eq("#{@cmm1.id}")
     expect(result[:data][:carMonthlyMileage][:month]).to eq(@cmm1.month)
     expect(result[:data][:carMonthlyMileage][:year]).to eq(@cmm1.year)
     expect(result[:data][:carMonthlyMileage][:totalMileage]).to eq(@cmm1.total_mileage)
+    expect(result[:data][:carMonthlyMileage][:car]).to have_key(:id)
+    expect(result[:data][:carMonthlyMileage][:car][:id]).to eq(@car.id)
+    expect(result[:data][:carMonthlyMileage][:car]).to have_key(:make)
+    expect(result[:data][:carMonthlyMileage][:car][:make]).to eq(@car.make)
+    expect(result[:data][:carMonthlyMileage][:car]).to have_key(:model)
+    expect(result[:data][:carMonthlyMileage][:car][:model]).to eq(@car.model)
+    expect(result[:data][:carMonthlyMileage][:car]).to have_key(:year)
+    expect(result[:data][:carMonthlyMileage][:car][:year]).to eq(@car.year)
+    expect(result[:data][:carMonthlyMileage][:car]).to have_key(:mpg)
+    expect(result[:data][:carMonthlyMileage][:car][:mpg]).to eq(@car.mpg)
+    expect(result[:data][:carMonthlyMileage][:car]).to have_key(:fuelType)
+    expect(result[:data][:carMonthlyMileage][:car][:fuelType]).to eq(@car.fuel_type)
   end
 end
